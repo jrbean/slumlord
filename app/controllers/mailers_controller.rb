e81aac14bfd0ce915035370fd14a0b302d1f253d
Justin Bean <beanjustinr@gmail.com>
2015-11-06 16:09:56 -0500

    correct mailer method

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -14,8 +14,16 @@ class MailersController < ApplicationController
   end
 
   def tenant_mail
-    @recipient_email = current_user.management_mail.join(", ")
-    body = params[:mail_body]
-    Mailer.invitation(@recipient_email).deliver
+    if current_user
+      if current_user.is_manager == false || nil
+        @recipient_email = current_user.management_mail.join(", ")
+        body = params[:mail_body]
+        Mailer.invitation(@recipient_email).deliver
+      else
+        render json: { error: "Only tenants can do that."}
+      end
+    else
+      render json: { error: "You must login."}
+    end
   end
 end
